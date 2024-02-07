074137a61a15f44a3d0dbabead8053d1c8e813d9
Naser Traiji <Nasertraiji@gmail.com>
2024-02-07 14:34:15 +0000

    aggregation

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,6 +1,7 @@
 select
 date_date
-,sum(ads_cost)as ads_cost
+,sum(ads_cost) as ads_cost
 ,sum(impression) as ads_impression
-sum(click) as ads_clicks
-from
+,sum(click) as ads_clicks
+from {{ ref('int_campaigns') }}
+group by date_date
